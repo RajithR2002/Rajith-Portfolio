35b91a3d0bc50a266291054b36fe0ca2e41ebbae
RajithR2002 <RajithR2002@outlook.com>
2025-12-13 14:38:27 -0500

    new color

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -19,7 +19,7 @@ return (
                 <h6 class="card-subtitle text-center mb-3">Founder and Manager</h6>
 
                 <p class="card-text text-center">
-                    Built and managed a <a class="text-reset" href="https://www.roblox.com/communities/2971038/Dawn-Of-Heroes-Community#!/about">community</a> of over <strong>125.4K members</strong>. Led communication, posted development updates, and maintained long-term engagement around the Dawn Of Heroes franchise.
+                    Built and managed a <a class="text-reset" href="https://www.roblox.com/communities/2971038/Dawn-Of-Heroes-Community#!/about"><strong>community</strong></a> of over <strong>125.4K members</strong>. Led communication, posted development updates, and maintained long-term engagement around the Dawn Of Heroes franchise.
                 </p>
 
                 <p class="card-text text-center"><small>Since 2015</small></p>
@@ -44,7 +44,7 @@ return (
     </div>
 
     <div class="mb-4">
-        <div class="card bg-primary text-light">
+        <div class="card video-card text-light">
             <div class="card-video-container">
                 <iframe
                     class="card-img-top"
@@ -69,13 +69,13 @@ return (
     </div>
 
     <div class="mb-4">
-        <div class="card text-dark">
+        <div class="card game text-dark">
             <img class="card-img-top masonry-image" loading="auto" src={require('../assets/img/dawn-of-heroes.jpg')} alt="Dawn Of Heroes Cover Art" />
             <div class="card-body">
                 <h5 class="card-title text-center">Dawn of Heroes</h5>
                 <h6 class="card-subtitle text-center mb-3 text-muted">An Open-World PvP Superhero Game</h6>
                 <p class="card-text text-center">
-                    <a class="text-reset" href="https://www.roblox.com/games/262050135/Dawn-Of-Heroes">Dawn of Heroes</a> has been played <strong>33 million</strong> times and has been featured on Roblox multiple times.
+                    <a class="text-reset" href="https://www.roblox.com/games/262050135/Dawn-Of-Heroes"><strong>Dawn of Heroes</strong></a> has been played <strong>33 million</strong> times and has been featured on Roblox multiple times.
                 </p>
                 <p class="card-text text-center"><small class="text-muted">Active from 2015 to 2019</small></p>
             </div>
@@ -98,7 +98,7 @@ return (
     </div>
 
     <div class="mb-4">
-        <div class="card text-dark">
+        <div class="card game text-dark">
         <div class="card-video-container">
                 <iframe
                     class="card-img-top"
@@ -113,7 +113,7 @@ return (
                 <h5 class="card-title text-center">Car Chaos</h5>
                 <h6 class="card-subtitle text-center mb-3 text-muted">An round-based car game with powers and random events</h6>
                 <p class="card-text text-center">
-                    <a class="text-reset" href="https://www.roblox.com/games/17687972564/Car-Chaos">Car Chaos</a> was designed, built and deployed in <strong>2 months</strong> to test my development speed and production discipline.
+                    <a class="text-reset" href="https://www.roblox.com/games/17687972564/Car-Chaos"><strong>Car Chaos</strong></a> was designed, built and deployed in <strong>2 months</strong> to test my development speed and production discipline.
                 </p>
                 <p class="card-text text-center"><small class="text-muted">Since October 2024</small></p>
             </div>
@@ -127,7 +127,7 @@ return (
             <h5 class="card-title text-center">Invision Games</h5>
             <h6 class="card-subtitle text-center mb-3">Software Developer Intern</h6>
             <p class="card-text text-center">
-                Developed a full-feature production update for a <a class="text-reset" href="https://www.roblox.com/games/103072644129928/Project-Power">live game</a> with <strong>34M+ visits</strong>, including UI, backend/frontend logic, and 3D assets. Enhanced player experience with a new cosmetic system.
+                Developed a full-feature production update for a <a class="text-reset" href="https://www.roblox.com/games/103072644129928/Project-Power"><strong>live game</strong></a> with <strong>34M+ visits</strong>, including UI, backend/frontend logic, and 3D assets. Enhanced player experience with a new cosmetic system.
             </p>
             <p class="card-text text-center"><small>October 2025</small></p>
             </div>
@@ -150,7 +150,7 @@ return (
     </div>
 
     <div class="mb-4">
-        <div class="card bg-primary text-light">
+        <div class="card video-card text-light">
             <div class="card-video-container">
                 <iframe
                     class="card-img-top"
